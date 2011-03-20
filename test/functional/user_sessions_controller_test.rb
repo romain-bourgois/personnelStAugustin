e9d4931eb22c4a57ed0b3238526e7604eb526313
Romain Bourgois <romain.bourgois@gmail.com>
2011-03-20 14:09:30 +0100

    Correction test

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -8,8 +8,19 @@ class UserSessionsControllerTest < ActionController::TestCase
   end
   
   def test_create
-    post :create
+    user = User.new :login => 'toto', :password => 'tata', :password_confirmation => 'tata'
+    user_session = UserSession.stubs :user => user
+    UserSession.stubs(:new).with('login' => 'toto', 'password' => 'tata').returns(user_session)
+    user_session.expects :save!
+    post :create, :user_session => {:login => 'toto', :password => 'tata'}
     assert_redirected_to root_path
   end
+  
+  def test_create_mauvais_leve_une_exception
+      post :create, :user_session => {:login => 'fake', :password => 'fake_pass'}
+      assert_template :new
+      assert_response :success
+  end
+  
 
 end
